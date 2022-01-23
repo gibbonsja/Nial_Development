b2b920b1da7d71cfe7bf15071f6b9bc912da33ef
John Gibbons <john.gibbons@xsellreward.com>
2022-01-23 17:23:13 +1100

    Razetime changes to site

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Welcome to Nial_Development",
         "excerpt":"Welcome to the new Nial development website.   This site is a fork of the QNial7 distribution of Nial and is intended to be a base for further development of the Nial programming language.  ","categories": [],
         "tags": ["Nial"],
-        "url": "/Nial_Development/2021-10-24-welcome-to-nial/welcome-to-nial.html",
+        "url": "/2021-10-24-welcome-to-nial/welcome-to-nial/html",
         "teaser": null
       }]
